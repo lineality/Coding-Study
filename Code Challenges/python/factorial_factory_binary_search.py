c61a086bd8ea091d4eae99fbf3433b0e660dcc46
oops <oops@localhost.localdomain>
2020-10-20 12:22:53 -0400

    new

---
+++ Code Challenges/python/factorial_factory_binary_search.py
@@ -0,0 +1,67 @@
+# https://binarysearch.com/room/stack-smashers-4h7zTJbeDi
+#
+# by GGA 2020.10.20
+# 
+# Problem (User's)
+# Have: one number
+# Need: factorial product of that number
+# Must: return one integer
+
+# Solution:
+# For a recursive solution...
+#
+# First, establish your base case: when you cannot reduce any more, and so n=1 
+# (n =1 is the base case)
+# 
+# For a recursive solution, you have a number of recursive steps equal to the input integer:
+#
+# For example, input = 4, has 4 steps
+#
+# n =  4
+# n =  3
+# n =  2
+# Base Case! n =  1
+# -> 24
+# 
+# Once the base case is reached, each step (a stack of un-computed steps) unwinds
+# and they are all computed at the end:
+#
+# Set up / creation order:
+# 4th 3rd  2nd   1st
+#
+# # Compute order:
+# 1st 2nd  3rd   4th
+#
+# n * (n * (n  * (n) ) )
+# 1 * (2 * (3  * (4) ) )
+# -> 24
+#
+# making a stack of N's to solve
+# from 'top' to 'bottom'
+# 
+# | 1 * | 
+# | 2 * | 
+# | 3 * | 
+# | 4 | 
+# | return |
+#
+
+
+def solve(n):
+    # base case
+    if n == 1:
+
+        # # inspection
+        # print("Base Case! n = ", n)
+
+        return n
+
+    else:  # each time, multiply the number times solve(n - 1)
+
+        # # inspection
+        # print("n = ", n)
+
+        return n * solve(n - 1)
+
+
+print(solve(4))
