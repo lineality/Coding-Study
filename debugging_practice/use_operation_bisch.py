56b25511b5f1456e731095599010312911456b20
oops <oops@localhost.localdomain>
2020-10-21 11:22:30 -0400

    new

--- debugging_practice/use_operation_bisch.py
+++ debugging_practice/use_operation_bisch.py
@@ -1,3 +1,13 @@
+# https://binarysearch.com/room/stack-smashers-4h7zTJbeDi
+
+# Given a list of integers nums, a string op representing
+# either "+", "-", "/", or "*", and an integer val, perform
+# the operation on every number in nums with val and return
+# the result.
+
+# Note: "/" is integer division.
+
+
 class Solution:
     def solve(self, nums, op, val):
         answer = [x + val for x in nums if op == "+"]
@@ -5,6 +15,22 @@ class Solution:
         answer = [x // val for x in nums if op == "/"]
         answer = [x - val for x in nums if op == "-"]
         return answer
+
+
+Tom = Solution()
+Tom.solve([3, 1, 6], "-", 4)
+
+# or
+
+
+class Solution:
+    def solve(self, nums, op, val):
+        return (
+            [x + val for x in nums if op == "+"]
+            + [x * val for x in nums if op == "*"]
+            + [x // val for x in nums if op == "/"]
+            + [x - val for x in nums if op == "-"]
+        )
 
 
 Tom = Solution()
